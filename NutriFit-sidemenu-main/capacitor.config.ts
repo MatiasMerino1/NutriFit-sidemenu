a5b264ac022eff595800223b8e6cbdb7210cbd8a
MatiasMerino1 <ma.merinon@duocuc.cl>
2024-10-09 01:10:05 -0300

    Delete NutriFit-sidemenu-main directory

--- NutriFit-sidemenu-main/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'ionic.nutrifit',
-  appName: 'nutrifit',
-  webDir: 'www'
-};
-
-export default config;
